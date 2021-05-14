3a15074b2063e8f7e890a390f931b602bd67856c
chris.conley@se.com <chris.conley@se.com>
2021-05-14 07:27:34 -0400

    initial

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+//https://www.youtube.com/watch?v=3dHNOWTI7H8&ab_channel=TraversyMedia
 
 if (environment.production) {
   enableProdMode();
